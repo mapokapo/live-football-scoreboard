8e85287b1bb1ade657e3cc0db76966ec202408cf
mapokapo <leopetrovic11@gmail.com>
2025-05-19 17:32:21 +0200

    refactor: remove unused import

--- src/main/java/me/mapokapo/features/matches/Match.java
+++ src/main/java/me/mapokapo/features/matches/Match.java
@@ -1,7 +1,6 @@
 package me.mapokapo.features.matches;
 
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import me.mapokapo.features.teams.Team;
 import lombok.AccessLevel;
