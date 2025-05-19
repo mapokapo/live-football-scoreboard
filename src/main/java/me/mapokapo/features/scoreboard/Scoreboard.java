679ca8da16d0fa6cb5d304b1cbf0a698a106f0e8
mapokapo <leopetrovic11@gmail.com>
2025-05-19 18:02:42 +0200

    feat(scoreboard): implement Scoreboard class

--- src/main/java/me/mapokapo/features/scoreboard/Scoreboard.java
+++ src/main/java/me/mapokapo/features/scoreboard/Scoreboard.java
@@ -1,6 +1,8 @@
 package me.mapokapo.features.scoreboard;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import me.mapokapo.features.matches.Match;
 import me.mapokapo.features.matches.MatchRepository;
@@ -29,14 +31,28 @@ public class Scoreboard {
 	}
 
 	/**
-	 * Finishes a match by ID. This method is simply a proxy for the
-	 * {@link MatchRepository#removeMatch(int)}
-	 * method.
+	 * Finishes a match by ID. This subsequently removes the match from the match
+	 * repository.
 	 * 
 	 * @param matchId The ID of the match to finish.
+	 * @throws IllegalArgumentException if the match is already finished or does not
+	 *                                  exist.
+	 * @throws IllegalStateException    if the match has not been started yet.
 	 */
 	public void finishMatch(int matchId) {
-		// TODO
+		Optional<Match> maybeMatch = matchRepository.getMatchById(matchId);
+
+		if (maybeMatch.isEmpty()) {
+			throw new IllegalArgumentException("Match already finished or does not exist");
+		}
+
+		Match match = maybeMatch.get();
+
+		if (!match.isStarted()) {
+			throw new IllegalStateException("Match has not started yet");
+		}
+
+		matchRepository.removeMatch(matchId);
 	}
 
 	/**
@@ -52,7 +68,25 @@ public class Scoreboard {
 	 * @return A list of matches sorted according to special criteria.
 	 */
 	public List<Match> getSummary() {
-		// TODO
-		return null;
+		// The returned list of matches is an unmodifiable view of the original,
+		// so we need to create a clone.
+		List<Match> matches = new ArrayList<>(matchRepository.getAllMatches());
+
+		// This method sorts in-place (does not create a new list), so the `matches`
+		// list needs to be modifiable, which is why we cloned it above.
+		matches.sort((match1, match2) -> {
+			int totalGoals1 = match1.getHomeScore() + match1.getAwayScore();
+			int totalGoals2 = match2.getHomeScore() + match2.getAwayScore();
+
+			if (totalGoals1 != totalGoals2) {
+				return Integer.compare(totalGoals2, totalGoals1);
+			}
+
+			// A simple way to sort by insertion order is to use the match ID, which
+			// increments with each new match.
+			return Integer.compare(match2.getId(), match1.getId());
+		});
+
+		return matches;
 	}
 }
