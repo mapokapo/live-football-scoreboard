59c08ad5889fba42f4219a5f73afbec4d71f942c
mapokapo <47316453+mapokapo@users.noreply.github.com>
2025-05-19 17:45:33 +0000

    Deploying to javadoc from @ mapokapo/live-football-scoreboard@704f2a71ac2a994f70945c5b1affc15f1010c968 🚀

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"me.mapokapo"},{"l":"me.mapokapo.features.matches"},{"l":"me.mapokapo.features.scoreboard"},{"l":"me.mapokapo.features.teams"}];updateSearchResults();
