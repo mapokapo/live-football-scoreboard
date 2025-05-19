21c6a8d3862e155cfda03c2664c35ad791b721e9
mapokapo <leopetrovic11@gmail.com>
2025-05-19 19:23:16 +0200

    feat(scoreboard): update signature of Scoreboard class to match documentation comment (methods: addMatch, startMatch, finishMatch, updateScore, getSummary) - the methods are mostly proxied to TeamRepository and MatchRepository

--- src/main/java/me/mapokapo/features/scoreboard/Scoreboard.java
+++ src/main/java/me/mapokapo/features/scoreboard/Scoreboard.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 
 import me.mapokapo.features.matches.Match;
 import me.mapokapo.features.matches.MatchRepository;
+import me.mapokapo.features.teams.Team;
+import me.mapokapo.features.teams.TeamRepository;
 
 /**
  * This class manages multiple matches. It functions as a repository for
@@ -17,60 +19,136 @@ import me.mapokapo.features.matches.MatchRepository;
  * 
  * <ul>
  * <li>Add a match</li>
- * <li>Get a match by ID</li>
- * <li>Get all matches</li>
- * <li>Finish a match by ID</li>
- * <li>Get a summary of all matches</li>
+ * <li>Start and finish a match</li>
+ * <li>Update the score of a live match</li>
+ * <li>Finish a match currently in progress, which hides it from the
+ * scoreboard summary</li>
+ * <li>Get a summary of all matches in progress in a special order</li>
  * </ul>
  */
 public class Scoreboard {
 	private final MatchRepository matchRepository;
+	private final TeamRepository teamRepository;
 
-	public Scoreboard(MatchRepository matchRepository) {
+	public Scoreboard(MatchRepository matchRepository, TeamRepository teamRepository) {
 		this.matchRepository = matchRepository;
+		this.teamRepository = teamRepository;
 	}
 
 	/**
-	 * Finishes a match by ID. This subsequently removes the match from the match
-	 * repository.
+	 * Adds a match to the scoreboard.
+	 * 
+	 * @note The default state of the newly created match is:
+	 *       <ul>
+	 *       <li>Not started</li>
+	 *       <li>Not finished</li>
+	 *       <li>Scores are set to 0</li>
+	 *       </ul>
+	 * 
+	 * @param homeTeam The home team.
+	 * @param awayTeam The away team.
+	 * @return The match object created.
+	 */
+	public Match addMatch(Team homeTeam, Team awayTeam) {
+		Match match = new Match(matchRepository.getNextIndex(), homeTeam, awayTeam);
+
+		matchRepository.addMatch(match);
+
+		return match;
+	}
+
+	public Match addMatch(String homeTeamName, String awayTeamName) {
+		Team homeTeam = teamRepository.addTeam(new Team(teamRepository.getNextIndex(), homeTeamName));
+		Team awayTeam = teamRepository.addTeam(new Team(teamRepository.getNextIndex(), awayTeamName));
+		Match match = new Match(matchRepository.getNextIndex(), homeTeam, awayTeam);
+
+		matchRepository.addMatch(match);
+
+		return match;
+	}
+
+	/**
+	 * Starts a match by ID. This allows the user to update the score of the match.
+	 * 
+	 * @param matchId The ID of the match to start.
+	 * @throws IllegalArgumentException if the match does not exist.
+	 * @throws IllegalStateException    if the match is already finished.
+	 * @throws IllegalStateException    if the match is already started.
+	 */
+	public void startMatch(int matchId) {
+		Optional<Match> maybeMatch = matchRepository.getMatchById(matchId);
+
+		if (maybeMatch.isEmpty()) {
+			throw new IllegalArgumentException("Match does not exist");
+		}
+
+		Match match = maybeMatch.get();
+		match.start();
+	}
+
+	/**
+	 * Finishes a match by ID. This hides the match from the scoreboard summary but
+	 * does not delete it.
 	 * 
 	 * @param matchId The ID of the match to finish.
-	 * @throws IllegalArgumentException if the match is already finished or does not
-	 *                                  exist.
+	 * @throws IllegalArgumentException if the match does not exist.
+	 * @throws IllegalStateException    if the match is already finished.
 	 * @throws IllegalStateException    if the match has not been started yet.
 	 */
 	public void finishMatch(int matchId) {
 		Optional<Match> maybeMatch = matchRepository.getMatchById(matchId);
 
 		if (maybeMatch.isEmpty()) {
-			throw new IllegalArgumentException("Match already finished or does not exist");
+			throw new IllegalArgumentException("Match does not exist");
 		}
 
-		Match match = maybeMatch.get();
-
-		if (!match.isStarted()) {
-			throw new IllegalStateException("Match has not started yet");
-		}
-
-		matchRepository.removeMatch(matchId);
+		var match = maybeMatch.get();
+		match.finish();
 	}
 
 	/**
-	 * Returns a specially-sorted list of matches which serves as a summary of the
-	 * current scoreboard.
+	 * Updates the score of a match by ID. This method can only be called if the
+	 * match is currently in progress.
+	 * 
+	 * @param matchId   The ID of the match to update.
+	 * @param homeScore The new home score.
+	 * @param awayScore The new away score.
+	 * @throws IllegalArgumentException if the match does not exist.
+	 * @throws IllegalStateException    If the match has not started yet.
+	 * @throws IllegalStateException    If the match has already finished.
+	 * @throws IllegalArgumentException If the score is negative.
+	 */
+	public void updateScore(int matchId, int homeScore, int awayScore) {
+		Optional<Match> maybeMatch = matchRepository.getMatchById(matchId);
+
+		if (maybeMatch.isEmpty()) {
+			throw new IllegalArgumentException("Match does not exist");
+		}
+
+		Match match = maybeMatch.get();
+		match.setScore(homeScore, awayScore);
+	}
+
+	/**
+	 * Returns a specially-sorted list of currently-running matches which serves as
+	 * a summary of the current scoreboard.
 	 * 
 	 * @note The summary is sorted according to the following criteria:
-	 *       - Matches are sorted by the number of goals scored in descending order
-	 *       (i.e. total goals).
-	 *       - If two matches have the same number of goals, the match which was
-	 *       inserted last is listed first.
+	 *       <ul>
+	 *       <li>Matches are sorted by the number of goals scored in descending
+	 *       order (i.e. total goals).</li>
+	 *       <li>If two matches have the same number of goals, the match which was
+	 *       inserted last is listed first.</li>
+	 *       </ul>
 	 * 
 	 * @return A list of matches sorted according to special criteria.
 	 */
 	public List<Match> getSummary() {
+		// Get all matches which are currently in progress (started but not finished).
 		// The returned list of matches is an unmodifiable view of the original,
 		// so we need to create a clone.
-		List<Match> matches = new ArrayList<>(matchRepository.getAllMatches());
+		List<Match> matches = new ArrayList<>(matchRepository.getAllMatches().stream()
+				.filter(match -> match.isStarted() && !match.isFinished()).toList());
 
 		// This method sorts in-place (does not create a new list), so the `matches`
 		// list needs to be modifiable, which is why we cloned it above.
